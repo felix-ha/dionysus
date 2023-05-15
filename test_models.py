c62d93ab2b6041c37d38e10655b42d770ddb562d
Felix Jobson <hauerf98@gmail.com>
2023-05-15 19:29:15 +0000

    check shape of embedding layer

--- test_models.py
+++ test_models.py
@@ -36,6 +36,25 @@ class TestBigramLanguageModel(unittest.TestCase):
             self.assertTrue(T == 5) # T_x = 5
             self.assertTrue(C == self.vocab_size)
 
+    def test_embedding(self):
+        x = torch.tensor([[1, 2, 3, 4, 5],
+                        [4, 23, 1, 0, 25],
+                        [22, 2, 4, 12, 19]]) 
+        
+        B, T = x.shape
+        self.assertTrue(B == 3)
+        self.assertTrue(T == 5)
+        
+        vocab_size = torch.max(x).item() + 1
+        self.assertTrue(vocab_size == 26) 
+
+        n_embd = 50
+        embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=n_embd)
+
+        out = embedding(x)
+
+        self.assertTrue(out.shape == (B, T, n_embd))
+
     def test_self_attention_head(self):
         B = 1
         n_embd = 26
@@ -47,6 +66,30 @@ class TestBigramLanguageModel(unittest.TestCase):
         out = head(x)
 
         self.assertTrue(out.shape == (B, block_size, head_size))
+
+    def test_embedding_self_attention(self):
+        x = torch.tensor([[1, 2, 3, 4, 5],
+                        [4, 23, 1, 0, 25],
+                        [22, 2, 4, 12, 19]]) 
+        
+        B, T = x.shape
+        self.assertTrue(B == 3)
+        self.assertTrue(T == 5)
+        
+        vocab_size = torch.max(x).item() + 1
+        self.assertTrue(vocab_size == 26) 
+
+        n_embd = 50
+        head_size = 25
+        block_size = T
+        embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=n_embd)
+        head = Head(head_size, n_embd, block_size)
+
+        emb = embedding(x)
+        out = head(emb)
+
+        self.assertTrue(emb.shape == (B, T, n_embd))
+        self.assertTrue(out.shape == (B, T, head_size))
 
 
 if __name__ == '__main__':
