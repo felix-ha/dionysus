23c37e192831af25868025cc0ef4683b40fdab29
Felix Jobson <hauerf98@gmail.com>
2023-05-17 21:56:58 +0200

    removed incremential models

--- test_examples.py
+++ test_examples.py
@@ -7,14 +7,6 @@ class Test(unittest.TestCase):
         feadforward_moon()
     def test_bigram(self):
         bigram()
-    def test_bigramV2(self):
-        bigramV2()
-    def test_bigramV3(self):
-        bigramV3()
-    def test_bigramV4(self):
-        bigramV4()
-    def test_bigramV5(self):
-        bigramV5()
     def test_bigramV6(self):
         bigramV6()
         
