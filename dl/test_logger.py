05fd878a19ba9a877ad246a93853c207799d7d98
Felix Jobson <hauerf98@gmail.com>
2023-06-29 21:38:59 +0200

    removed test logging file

--- dl/test_logger.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import logging
-
-
-
-def foo(path):
-    print(path.joinpath('info.log'))
-    logging.basicConfig(level=logging.INFO, filename=path.joinpath('info.log'))
-    logging.info("hi")
