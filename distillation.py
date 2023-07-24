153f7e1717747476e70699279f99e9349a1c04ef
Felix Jobson <hauerf98@gmail.com>
2023-07-24 21:54:54 +0200

    First draft of distillation loss
    Need to rewrite, batch needs to be passed to teacher model

---
+++ distillation.py
@@ -0,0 +1,109 @@
+import os
+import logging
+import torch
+import torch.nn as nn
+
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, TensorDataset
+
+from dl.training import train, TrainingConfig
+from dl.loss import DistilationLoss
+
+
+def get_dataloader(n_features, n_classes):
+    weights = [0.2, 0.3, 0.5]
+
+    X, y = make_classification(
+        n_samples=300,
+        n_features=n_features,
+        n_redundant=0,
+        n_classes=n_classes,
+        n_clusters_per_class=1,
+        n_informative=2,
+        class_sep=2,
+        random_state=123,
+        weights=weights,
+    )
+
+    X_train, X_validation, y_train, y_validation = train_test_split(
+        X, y, test_size=0.2, stratify=y, random_state=123
+    )
+
+    train_dataset = TensorDataset(
+        torch.tensor(X_train, dtype=torch.float32),
+        torch.tensor(y_train, dtype=torch.long),
+    )
+    validation_dataset = TensorDataset(
+        torch.tensor(X_validation, dtype=torch.float32),
+        torch.tensor(y_validation, dtype=torch.long),
+    )
+    training_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    validation_loader = DataLoader(validation_dataset, batch_size=16, shuffle=False)
+
+    return training_loader, validation_loader
+
+
+def train_teacher(n_features, n_classes, training_loader, validation_loader):
+    teacher = nn.Sequential(
+        nn.Linear(n_features, 16),
+        nn.ReLU(),
+        nn.Linear(16, 32),
+        nn.ReLU(),
+        nn.Linear(32, 32),
+        nn.ReLU(),
+        nn.Linear(32, 16),
+        nn.ReLU(),
+        nn.Linear(16, n_classes),
+    )
+
+    loss_func = nn.CrossEntropyLoss()
+
+    train_config = TrainingConfig(
+        model=teacher,
+        epochs=10,
+        loss_func=loss_func,
+        training_loader=training_loader,
+        validation_loader=validation_loader,
+        save_model=True,
+        save_path=os.path.join(os.getcwd(), "runs"),
+        model_name="teacher",
+        classification_metrics=True,
+        class_names=["A", "B", "C"],
+        progress_bar=True,
+        zip_result=False,
+    )
+
+    logging.info(f"start training of model: {train_config.model_name}")
+    train(train_config)
+
+    return train_config.model
+
+
+n_features = 2
+n_classes = 3
+
+training_loader, validation_loader = get_dataloader(n_features, n_classes)
+teacher = train_teacher(n_features, n_classes, training_loader, validation_loader)
+
+distilation_loss = DistilationLoss(teacher, temperature=2, alpha=0.5)
+
+student = nn.Sequential(nn.Linear(n_features, 8), nn.ReLU(), nn.Linear(8, n_classes))
+
+train_config = TrainingConfig(
+    model=student,
+    epochs=2,
+    loss_func=distilation_loss,
+    training_loader=training_loader,
+    validation_loader=validation_loader,
+    save_model=True,
+    save_path=os.path.join(os.getcwd(), "runs"),
+    model_name="student",
+    classification_metrics=True,
+    class_names=["A", "B", "C"],
+    progress_bar=True,
+    zip_result=False,
+)
+
+logging.info(f"start training of model: {train_config.model_name}")
+train(train_config)
--- dl/loss.py
+++ dl/loss.py
@@ -1,11 +1,45 @@
-import torch 
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 
-class CrossEntropyLoss():
+class DistilationLoss:
     """
-    Class is just used for testing. 
+    Class is just used for testing.
     """
+
+    def __init__(self, teacher, temperature, alpha):
+        self.teacher = teacher
+        self.temperature = temperature
+        self.alpha = alpha
+        self.loss_func = nn.CrossEntropyLoss()
+
+    def __call__(self, input, target):
+        return self.forward(input, target)
+
+    def forward(self, input, target):
+        self.teacher.eval()
+        with torch.no_grad():
+            teacher_logits = self.teacher(input)
+
+        teacher_sm = F.softmax(teacher_logits / self.temperature, dim=-1)
+        student_sm = F.softmax(input, dim=-1)
+
+        loss_kd = self.temperature**2 * F.kl_div(
+            teacher_sm, student_sm, log_target=True, reduction="sum"
+        )
+        loss_ce = self.loss_func(input, target)
+
+        loss = self.alpha * loss_ce + (1 - self.alpha) * loss_kd
+
+        return loss
+
+
+class CrossEntropyLoss:
+    """
+    Class is just used for testing.
+    """
+
     def __init__(self, reduction="mean"):
         match reduction:
             case "sum":
@@ -14,7 +48,7 @@ class CrossEntropyLoss():
                 self.reduction_func = torch.mean
             case _:
                 raise NotImplementedError(f"Case {reduction} not implemented")
-            
+
     def __call__(self, input, target):
         return self.forward(input, target)
 
