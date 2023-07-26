fd9dcfe84b9c22722b46d782e4ba45260517e5b6
Felix Jobson <hauerf98@gmail.com>
2023-07-26 22:48:47 +0200

    train mnist with dropout teacher

--- distillation.py
+++ distillation.py
@@ -5,71 +5,67 @@ import torch.nn as nn
 
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
+
 from torch.utils.data import DataLoader, TensorDataset
+from torchvision import datasets, transforms
 
 from dl.training import train, TrainingConfig, DistillationConfig
 from dl.loss import DistilationLoss
 
 
-def get_dataloader(n_features, n_classes):
-    weights = [0.2, 0.3, 0.5]
-
-    X, y = make_classification(
-        n_samples=300,
-        n_features=n_features,
-        n_redundant=0,
-        n_classes=n_classes,
-        n_clusters_per_class=1,
-        n_informative=2,
-        class_sep=2,
-        random_state=123,
-        weights=weights,
+def get_mnist_dataloader():
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
     )
 
-    X_train, X_validation, y_train, y_validation = train_test_split(
-        X, y, test_size=0.2, stratify=y, random_state=123
+    trainset = datasets.MNIST(
+        root=os.path.join(os.getcwd(), "data"),
+        train=True,
+        download=True,
+        transform=transform,
+    )
+    testset = datasets.MNIST(
+        root=os.path.join(os.getcwd(), "data"),
+        train=False,
+        download=True,
+        transform=transform,
     )
 
-    train_dataset = TensorDataset(
-        torch.tensor(X_train, dtype=torch.float32),
-        torch.tensor(y_train, dtype=torch.long),
+    train_loader = DataLoader(
+        trainset, batch_size=128, shuffle=True
     )
-    validation_dataset = TensorDataset(
-        torch.tensor(X_validation, dtype=torch.float32),
-        torch.tensor(y_validation, dtype=torch.long),
+    test_loader = DataLoader(
+        testset, batch_size=128, shuffle=False
     )
-    training_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    validation_loader = DataLoader(validation_dataset, batch_size=16, shuffle=False)
 
-    return training_loader, validation_loader
+    return train_loader, test_loader
 
 
 def train_teacher(n_features, n_classes, training_loader, validation_loader):
     teacher = nn.Sequential(
-        nn.Linear(n_features, 16),
-        nn.ReLU(),
-        nn.Linear(16, 32),
-        nn.ReLU(),
-        nn.Linear(32, 32),
-        nn.ReLU(),
-        nn.Linear(32, 16),
-        nn.ReLU(),
-        nn.Linear(16, n_classes),
-    )
+    nn.Flatten(),
+    nn.Linear(n_features, 1200),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(1200, 1200),
+    nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(1200, n_classes),
+)
 
     loss_func = nn.CrossEntropyLoss()
 
     train_config = TrainingConfig(
         model=teacher,
-        epochs=10,
+        epochs=100,
         loss_func=loss_func,
         training_loader=training_loader,
         validation_loader=validation_loader,
         save_model=True,
         save_path=os.path.join(os.getcwd(), "runs"),
-        model_name="teacher",
+        model_name="teacher_dropout",
         classification_metrics=True,
-        class_names=["A", "B", "C"],
+        class_names=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
         progress_bar=True,
         zip_result=False,
     )
@@ -80,19 +76,21 @@ def train_teacher(n_features, n_classes, training_loader, validation_loader):
     return train_config.model
 
 
-n_features = 2
-n_classes = 3
+n_features = 784
+n_classes = 10
 
-training_loader, validation_loader = get_dataloader(n_features, n_classes)
+training_loader, validation_loader = get_mnist_dataloader()
+
+# TODO: find teacher that learns mnist
 teacher = train_teacher(n_features, n_classes, training_loader, validation_loader)
 
-student = nn.Sequential(nn.Linear(n_features, 8), nn.ReLU(), nn.Linear(8, n_classes))
+student = nn.Sequential(nn.Flatten(),nn.Linear(n_features, 16), nn.ReLU(), nn.Linear(16, n_classes))
 
 distilation_config = DistillationConfig(teacher=teacher, temperature=2, alpha=0.5)
 
 train_config = TrainingConfig(
     model=student,
-    epochs=2,
+    epochs=5,
     loss_func=None,
     training_loader=training_loader,
     validation_loader=validation_loader,
@@ -100,7 +98,7 @@ train_config = TrainingConfig(
     save_path=os.path.join(os.getcwd(), "runs"),
     model_name="student",
     classification_metrics=True,
-    class_names=["A", "B", "C"],
+    class_names=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     progress_bar=True,
     zip_result=False,
     distillation_config=distilation_config
