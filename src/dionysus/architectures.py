4a8fb4d1cf0f6675681763e7e3b099278ecba7f3
felix-ha <hauerf98@gmail.com>
2023-10-08 18:46:30 +0000

    Implement AlexNet for mnist

--- src/dionysus/architectures.py
+++ src/dionysus/architectures.py
@@ -26,3 +26,40 @@ class LeNet5(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
+
+
+class AlexNet(nn.Module):
+    """
+    AlexNet inspired architecture for MNIST dataset.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.feature = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 96, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(96, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=1),
+        )
+        self.logits = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(32 * 12 * 12, 2048),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(2048, 1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 10),
+        )
+
+    def forward(self, x):
+        x = self.feature(x)
+        x = torch.flatten(x, start_dim=1)
+        return self.logits(x)
--- tests/test_architectures.py
+++ tests/test_architectures.py
@@ -1,12 +1,19 @@
 import unittest
 import torch
 import torch.nn.functional as F
-from src.dionysus.architectures import LeNet5
+from src.dionysus.architectures import LeNet5, AlexNet
 
 
 class Test(unittest.TestCase):
     def test_lenet5(self):
         model = LeNet5()
+        input_tensor = torch.randn(1, 1, 28, 28)
+        output_tensor = model(input_tensor)
+        assert output_tensor.shape == (1, 10)
+        assert torch.allclose(F.softmax(output_tensor.sum(dim=1)), torch.ones(1))
+
+    def test_alexnet(self):
+        model = AlexNet()
         input_tensor = torch.randn(1, 1, 28, 28)
         output_tensor = model(input_tensor)
         assert output_tensor.shape == (1, 10)
