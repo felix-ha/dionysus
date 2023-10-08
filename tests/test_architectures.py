9ee8d9a32332a6c78ff196ebf5922b38ade44deb
felix-ha <hauerf98@gmail.com>
2023-10-08 18:54:10 +0000

    Refactored test

--- tests/test_architectures.py
+++ tests/test_architectures.py
@@ -1,20 +1,19 @@
 import unittest
+import pytest
 import torch
 import torch.nn.functional as F
 from src.dionysus.architectures import LeNet5, AlexNet
 
 
 class Test(unittest.TestCase):
-    def test_lenet5(self):
-        model = LeNet5()
+    def run_inference_model(self, model):
         input_tensor = torch.randn(1, 1, 28, 28)
         output_tensor = model(input_tensor)
         assert output_tensor.shape == (1, 10)
         assert torch.allclose(F.softmax(output_tensor.sum(dim=1)), torch.ones(1))
 
+    def test_lenet5(self):
+        self.run_inference_model(LeNet5())
+
     def test_alexnet(self):
-        model = AlexNet()
-        input_tensor = torch.randn(1, 1, 28, 28)
-        output_tensor = model(input_tensor)
-        assert output_tensor.shape == (1, 10)
-        assert torch.allclose(F.softmax(output_tensor.sum(dim=1)), torch.ones(1))
+        self.run_inference_model(AlexNet())
