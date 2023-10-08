22c39b3e95eb3fe7e07df6c778d40362726f7e68
felix-ha <hauerf98@gmail.com>
2023-10-08 18:26:49 +0000

    Update training script for colab

--- training_scripts/colab/mnist.py
+++ training_scripts/colab/mnist.py
@@ -1,10 +1,12 @@
-# !pip install dionysus-trainer
+# !pip install git+https://github.com/felix-ha/dionysus-trainer.git@implementation
 
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 from torch.utils.data import DataLoader
+
 from dionysus.training import train, TrainingConfig
+from dionysus.architectures import LeNet5
 
 
 def get_mnist_datasets():
@@ -59,30 +61,11 @@ def get_mnist_datasets():
     return training_dataset, validation_dataset
 
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return x
-
-
 training_dataset, validation_dataset = get_mnist_datasets()
 training_loader = DataLoader(training_dataset, batch_size=512, shuffle=True)
 validation_loader = DataLoader(validation_dataset, batch_size=512)
 
-model = Net()
+model = LeNet5()
 loss_func = nn.CrossEntropyLoss()
 
 save_path = "runs"
@@ -101,7 +84,7 @@ train_config = TrainingConfig(
     class_names=[str(i) for i in range(0, 10)],
     zip_result=True,
     save_path=save_path,
-    model_name="mnist",
+    model_name="LeNet-5",
     progress_bar=False,
 )
 
